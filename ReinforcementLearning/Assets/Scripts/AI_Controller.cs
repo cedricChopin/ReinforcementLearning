7806771c77481163ab998ed73132d17f7f74eef7
cedricChopin <72026207+cedricChopin@users.noreply.github.com>
2022-02-20 19:12:02 +0100

    Correction Madeline

--- ReinforcementLearning/Assets/Scripts/AI_Controller.cs
+++ ReinforcementLearning/Assets/Scripts/AI_Controller.cs
@@ -7,23 +7,26 @@ public class AI_Controller : MonoBehaviour
     public List<Vector2> way;
     public Strategy_Policy policy;
     public GridManager grid;
+    private Vector2 currentPos;
 
     [SerializeField]
     private bool activated = false;
 
     float time = 2f;
     int i = 0;
+    bool move = false;
 
-    private void Start()
+    public void Start()
     {
         transform.position = grid.GetTileAtPosition(new Vector2(0,0)).transform.position;
+        currentPos = Vector2.zero;
     }
 
     public void LaunchAI()
     {
         way = new List<Vector2>();
-        State currentState =  policy.States[0];
-        Vector2 currentPos = Vector2.zero;
+        
+        State currentState = policy.States[(int)currentPos.x + (int)currentPos.y * grid.height];
 
         while(currentState.action != Action.Win)
         {
@@ -50,19 +53,27 @@ public class AI_Controller : MonoBehaviour
     public void ActivatedAI()
     {
         LaunchAI();
+        i = 0;
         activated = true;
     }
 
-    private void Update()
+    public void Update()
     {
         if (activated == true)
         {
             time -= Time.deltaTime;
+            if (move)
+                transform.position = Vector3.Lerp(transform.position, new Vector3(way[i].x, way[i].y, 0), Time.deltaTime * 2);
+
+            if (Vector3.Distance(transform.position, way[i]) < 0.02 && move)
+                move = false;
             if (time < 0 && i < way.Count)
             {
-                transform.position = way[i];
+                //transform.position = way[i];
                 time = 2f;
                 i++;
+                move = true;
+                
             }
             if (i >= way.Count) activated = false;
         }
